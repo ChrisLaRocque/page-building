75763f0e8b2d174e0e5dfe4ba401e03a33cde4a3
ChrisLaRocque <larocque.christopher@gmail.com>
2024-05-20 19:00:08 -0400

    Add typegen

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,24 +1,25 @@
 import { QueryParams, SanityDocument, groq } from "next-sanity";
 import { notFound } from "next/navigation";
 import { sanityFetch } from "@/sanity/lib/fetch";
+import { AllPageQueryResult, PageQueryResult } from "@/sanity/types";
 
 const allPageQuery = groq`*[_type == "page"]`;
 const pageQuery = groq`*[_type == "page" && slug.current == $slug][0]`;
 
 export async function generateStaticParams() {
-  const posts = await sanityFetch<SanityDocument[]>({
+  const posts = await sanityFetch<AllPageQueryResult>({
     query: allPageQuery,
     perspective: "published",
     stega: false,
   });
 
   return posts.map((page) => ({
-    slug: page.slug.current,
+    slug: page?.slug?.current,
   }));
 }
 
 export default async function Page({ params }: { params: QueryParams }) {
-  const page = await sanityFetch<SanityDocument>({ query: pageQuery, params });
+  const page = await sanityFetch<PageQueryResult>({ query: pageQuery, params });
   if (!page) {
     return notFound();
   }
